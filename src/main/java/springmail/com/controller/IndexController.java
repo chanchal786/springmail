2c912a83e45fed709320f823329bcd3631f35deb
chanchal kumar <chanchalshahkumar@gmail.com>
2019-02-20 22:12:39 +0530

    first commit

---
+++ src/main/java/springmail/com/controller/IndexController.java
@@ -0,0 +1,15 @@
+package springmail.com.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping("/")
+	public String userForm(Model model) {
+		System.out.println("Hi Controller");
+		return "default";
+	}
+}
---
+++ src/main/java/springmail/com/webappconfig/WebAppConfig.java
@@ -0,0 +1,38 @@
+package springmail.com.webappconfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages= {"springmail.com"})
+public class WebAppConfig implements WebMvcConfigurer{
+	
+	@Bean
+	public InternalResourceViewResolver resolver() {
+		InternalResourceViewResolver resolver=new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+	
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
+    }
+	
+	@Bean(name = "multipartResolver")
+	public CommonsMultipartResolver getCommonsMultipartResolver() {
+	    CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+	    multipartResolver.setMaxUploadSize(20971520);   // 20MB
+	    multipartResolver.setMaxInMemorySize(1048576);  // 1MB
+	    return multipartResolver;
+	}
+}
