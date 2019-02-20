38bf7b0d148cc3be9a346b129a36d1c1afc7c56e
chanchal kumar <chanchalshahkumar@gmail.com>
2019-02-20 22:27:17 +0530

    Initial files

--- src/main/java/springmail/com/webappconfig/WebAppConfig.java
+++ src/main/java/springmail/com/webappconfig/WebAppConfig.java
@@ -27,6 +27,7 @@ public class WebAppConfig implements WebMvcConfigurer{
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
         registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
     }
+
 	
 	@Bean(name = "multipartResolver")
 	public CommonsMultipartResolver getCommonsMultipartResolver() {
